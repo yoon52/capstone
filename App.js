dd7e4239f73d7355a40d2dac6e7e8332a00a9761
SEUNGH00N <93211110+SEUNGH00N@users.noreply.github.com>
2024-05-11 20:49:57 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -4,20 +4,30 @@ import Login from '../components/auth/Login';
 import Signup from '../components/auth/Signup';
 import FindId from '../components/auth/FindId';
 import FindPw from '../components/auth/FindPw';
-import NaverLogin from '../components/auth/NaverLogin';
-import NaverCallback from '../components/auth/NaverCallback';
-import KakaoLogin from '../components/auth/KakaoLogin';
-import KakaoCallback from '../components/auth/KakaoCallback';
+
 import Main from '../components/auth/Main';
+import AdminPage from '../components/auth/AdminPage';
 import SearchKeyword from '../components/auth/SearchKeyword';
-import AddProducts from '../components/auth/AddProducts';
+import SearchResults from '../components/auth/SearchResults';
+import AddProducts from '../components/auth/AddProductsPage';
 import ProductDetail from '../components/auth/ProductDetail';
+import ProductList from '../components/auth/ProductList';
+import LatestList from '../components/auth/LatestList';
+import DetailList from '../components/auth/DetailList';
+import Detail from '../components/auth/Detail';
+
 import ProductManagement from '../components/auth/ProductManagement';
 import MyInfo from '../components/auth/MyInfo';
-import PasswordChange from '../components/auth/PasswordChange';
-
-
+import ChangePw from '../components/auth/ChangePw';
+import ChatListComponent from '../components/auth/ChatListComponent';
+import ChatModal from '../components/auth/ChatModal';
+import { CheckoutPage } from '../components/auth/Checkout';
+import { SuccessPage } from '../components/auth/Success';
+import { FailPage } from '../components/auth/Fail';
+import ShowWishlist from '../components/auth/ShowWishlist'; // showWishlist 컴포넌트 파일 import
+import Payments from '../components/auth/Payments';
 import '../styles/main.css';
+
 
 function App() {
   return (
@@ -29,19 +39,34 @@ function App() {
           <Route path="/Signup" element={<Signup />} />
           <Route path="/FindId" element={<FindId />} />
           <Route path="/FindPw" element={<FindPw />} />
-          <Route path="/Naverlogin" element={<NaverLogin />} />
-          <Route path="/NaverCallback" element={<NaverCallback />} />
-          <Route path="/Kakaologin" element={<KakaoLogin />} />
-          <Route path="/KakaoCallback" element={<KakaoCallback />} />
+
           <Route path="/Main/*" element={<Main />} />
+          <Route path="/AdminPage" element={<AdminPage />} />
+
           <Route path="/SearchKeyword" element={<SearchKeyword />} />
+          <Route path="/SearchResults" element={<SearchResults />} />
+          <Route path="/searchResultsP/:searchTerm" element={<SearchResults />} />
+
           <Route path="/AddProducts" element={<AddProducts />} />
           <Route path="/ProductDetail/:productId" element={<ProductDetail />} />
           <Route path="/ProductManagement" element={<ProductManagement />} />
+          <Route path="/ProductList" element={<ProductList />} />
+          <Route path="/LatestList" element={<LatestList />} />
+          <Route path="/DetailList" element={<DetailList />} />
+          <Route path="/Detail" element={<Detail />} />
           <Route path="/MyInfo" element={<MyInfo />} />
-          <Route path="/ChangePw" element={<PasswordChange />} />
-
+          <Route path="/ChangePw" element={<ChangePw />} />
+          <Route path="/ChatListComponent" element={<ChatListComponent />} />
           {/* 다른 페이지 라우팅을 추가할 수 있습니다 */}
+          <Route exact path="/chat/:roomId" component={ChatModal} />
+          <Route path="/ShowWishlist" element={<ShowWishlist />} />
+          <Route path="/sandbox" element={<CheckoutPage />} />
+          <Route path="/sandbox/success" element={<SuccessPage />} />
+          <Route path="/sandbox/fail" element={<FailPage />} />
+
+          <Route path="/payments" element={<Payments />} />
+
+
         </Routes>
       </div>
     </Router>
