fd871eca182b4aeecd26b754cb3a1df78742ef4c
SEUNGH00N <93211110+SEUNGH00N@users.noreply.github.com>
2024-05-28 10:07:18 +0900

    Add files via upload

---
+++ components/auth/Main.jsx
@@ -0,0 +1,183 @@
+/* eslint-disable react/jsx-no-duplicate-props */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState, useRef } from 'react';
+import { useNavigate, Routes, Route } from 'react-router-dom';
+import ViewsList from '../products/ViewsList';
+import SearchResults from '../header/SearchResults';
+import ProductDetail from '../products/ProductDetail';
+import ProductManagement from '../products/ProductManagement';
+import ChatListComponent from '../messages/ChatListComponent';
+import Header from '../header/Header';
+import ShowWishlist from '../products/ShowWishlist';
+import '../../styles/main.css';
+import '../../styles/product.css';
+import serverHost from '../../utils/host';
+
+function Main() {
+  const [, setFilteredProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [, setSavedSearchTerm] = useState('');
+  const [, setShowSearchResults] = useState(false);
+  const [showNavMenu, setShowNavMenu] = useState(false);
+  const [searchError, setSearchError] = useState('');
+  const navigate = useNavigate();
+  const searchInputRef = useRef(null);
+  const [, setShowRecentSearches] = useState(false);
+
+  const handleAddProduct = () => {
+    navigate('/AddProducts');
+  };
+
+  const handleSearchProduct = async () => {
+    if (!searchTerm) {
+      setSearchError('검색어를 입력하세요.');
+      console.log('touch'); // 검색 인풋창 클릭시 "touch"를 콘솔에 출력
+      return;
+    }
+
+    try {
+      const response = await fetch(`${serverHost}:4000/products?search=${searchTerm}`);
+      if (response.ok) {
+        const data = await response.json();
+        setFilteredProducts(data);
+        setSavedSearchTerm(searchTerm);
+        saveSearchTerm(searchTerm);
+        setShowSearchResults(true);
+        setSearchError('');
+
+        // Navigate to the search results page
+        navigate(`/searchResultsP/${encodeURIComponent(searchTerm)}`);
+
+      } else {
+        console.error('검색 오류:', response.status);
+      }
+    } catch (error) {
+      console.error('검색 오류:', error);
+    }
+    // 검색어가 유효할 때 콘솔에 검색어 출력
+    console.log("검색어:", searchTerm);
+
+  };
+
+  const handleEnterKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearchProduct();
+    }
+  };
+
+  const saveSearchTerm = async (searchTerm) => {
+    try {
+      const userId = sessionStorage.getItem('userId');
+      const response = await fetch(`${serverHost}:4000/searchHistory`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ searchTerm, userId })
+      });
+      if (!response.ok) {
+        console.error('검색어 저장 오류:', response.status);
+      }
+    } catch (error) {
+      console.error('검색어 저장 오류:', error);
+    }
+  };
+
+  const handleChangeSearchTerm = (event) => {
+    setSearchTerm(event.target.value);
+    setSearchError('');
+  };
+
+  const handleKeywordManagement = () => {
+    navigate('/SearchKeyword');
+  };
+
+  const handleProductManagement = () => {
+    navigate('/ProductManagement');
+  };
+  const handleShowWishlist = () => {
+    navigate('/ShowWishlist');
+  };
+  const handleShowMyInfoPage = () => {
+    navigate('/MyInfo');
+  };
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('userId');
+    navigate('/Login');
+  };
+
+  const handleShowChatList = () => {
+    navigate('/ChatListComponent');
+  };
+
+  const toggleNavMenu = () => {
+    setShowNavMenu(!showNavMenu);
+  };
+
+  const closeNavMenu = () => {
+    setShowNavMenu(false);
+  };
+
+  const handleMoreList = () => {
+    navigate('/LatestList');
+  }
+
+  return (
+    <div className="container-main">
+      <Header
+        toggleNavMenu={toggleNavMenu}
+        showNavMenu={showNavMenu}
+        closeNavMenu={closeNavMenu}
+        handleAddProduct={handleAddProduct}
+        handleShowChatList={handleShowChatList}
+        handleShowMyInfoPage={handleShowMyInfoPage}
+        handleKeywordManagement={handleKeywordManagement}
+        handleProductManagement={handleProductManagement}
+        handleLogout={handleLogout}
+        searchTerm={searchTerm}
+        handleChangeSearchTerm={handleChangeSearchTerm}
+        handleEnterKeyPress={handleEnterKeyPress}
+        searchInputRef={searchInputRef}
+        handleShowWishlist={handleShowWishlist}
+        setShowRecentSearches={setShowRecentSearches}
+        userInfo
+        onSearchSubmit={handleSearchProduct}
+        recentSearches={[]}
+      />
+      <div className="main">
+        <section className="fleamarket-cover">
+          <div className="cover-content">
+            <h1 className="cover-title">믿을만한<br />교내 중고거래</h1>
+            <span className="cover-description">학생들과 가깝고 따뜻한 거래를<br />지금 경험해보세요.</span>
+            <div className="cover-image">
+              <span className="fleamarket-cover-image">
+                <img src="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/fleamarket-39d1db152a4769a6071f587fa9320b254085d726a06f61d544728b9ab3bd940a.webp " alt="믿을만한 이웃 간 중고거래" />
+              </span>
+            </div>
+          </div>
+        </section>
+        <div className="list-container">
+          <ViewsList
+          />
+        </div>
+        <div className="more-list">
+          <button className="more-button" onClick={handleMoreList}>전체 상품 보기</button>
+        </div>
+        <Routes>
+          <Route path="/ProductDetail/:productId" element={<ProductDetail />} />
+          <Route path="/ProductManagement" element={<ProductManagement />} />
+          <Route path="/ChatListComponent" element={<ChatListComponent />} />
+          <Route path="/showWishlist" element={<ShowWishlist />} />
+          <Route path="/SearchResults/:searchTerm" element={<SearchResults />} />
+        </Routes>
+        {searchError && (
+          <p className="search-error">{searchError}</p>
+        )}
+      </div>
+      {showNavMenu && <div className="overlay" onClick={closeNavMenu}></div>}
+    </div>
+  );
+}
+
+export default Main;
---
+++ components/header/SearchKeyword.jsx
@@ -0,0 +1,194 @@
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../../styles/searchkeyword.css';
+import serverHost from '../../utils/host';
+import Header from '../header/Header';
+
+function SearchKeyword() {
+  const [searchKeywords, setSearchKeywords] = useState([]);
+  const [userId, setUserId] = useState('');
+  const navigate = useNavigate();
+  const searchInputRef = useRef(null);
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [, setSavedSearchTerm] = useState('');
+  const [, setShowSearchResults] = useState(false);
+  const [showNavMenu, setShowNavMenu] = useState(false);
+  const [, setSearchError] = useState('');
+  const [, setFilteredProducts] = useState([]);
+
+  // 현재 로그인된 사용자의 ID를 가져오는 함수
+  const fetchUserId = useCallback(() => {
+    const userId = sessionStorage.getItem('userId');
+    setUserId(userId);
+  }, []);
+
+  // 검색어 목록을 서버로부터 가져오는 함수
+  const fetchSearchKeywords = useCallback(async () => {
+    try {
+      const response = await fetch(`${serverHost}:4000/SearchKeywords/${userId}`);
+      if (response.ok) {
+        const data = await response.json();
+        setSearchKeywords(data);
+      } else {
+        console.error('검색어를 가져오는 데 실패했습니다.');
+      }
+    } catch (error) {
+      console.error('검색어를 가져오는 중 오류가 발생했습니다:', error);
+    }
+  }, [userId]);
+
+  // 검색어 삭제 함수
+  const deleteKeyword = async (keywordId) => {
+    try {
+      const response = await fetch(`${serverHost}:4000/SearchKeywords/delete/${keywordId}`, {
+        method: 'DELETE'
+      });
+      if (response.ok) {
+        fetchSearchKeywords(); // 삭제 후 목록을 다시 가져옴
+      } else {
+        console.error('검색어 삭제에 실패했습니다.');
+      }
+    } catch (error) {
+      console.error('검색어를 삭제하는 중 오류가 발생했습니다:', error);
+    }
+  };
+
+  const handleSearchProduct = async () => {
+    if (!searchTerm) {
+      setSearchError('검색어를 입력하세요.');
+      return;
+    }
+    try {
+      const response = await fetch(`${serverHost}:4000/products?search=${searchTerm}`);
+      if (response.ok) {
+        const data = await response.json();
+        setFilteredProducts(data);
+        setSavedSearchTerm(searchTerm);
+        saveSearchTerm(searchTerm);
+        setShowSearchResults(true);
+        setSearchError('');
+
+        // Navigate to ResultPage with encoded searchTerm
+        navigate(`/SearchResultsP/${encodeURIComponent(searchTerm)}`);
+      } else {
+        console.error('검색 오류:', response.status);
+      }
+    } catch (error) {
+      console.error('검색 오류:', error);
+    }
+  };
+
+  const saveSearchTerm = async (searchTerm) => {
+    try {
+      const userId = sessionStorage.getItem('userId');
+      const response = await fetch(`${serverHost}:4000/searchHistory`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ searchTerm, userId })
+      });
+      if (!response.ok) {
+        console.error('검색어 저장 오류:', response.status);
+      }
+    } catch (error) {
+      console.error('검색어 저장 오류:', error);
+    }
+  };
+
+  const toggleNavMenu = () => {
+    setShowNavMenu(!showNavMenu);
+  };
+
+  const closeNavMenu = () => {
+    setShowNavMenu(false);
+  };
+
+  const handleAddProduct = () => {
+    navigate('/AddProducts');
+  };
+
+  const handleShowChatList = () => {
+    navigate('/ChatListComponent');
+  };
+
+  const handleKeywordManagement = () => {
+    navigate('/SearchKeyword');
+  };
+
+
+  const handleShowWishlist = () => {
+    navigate('/ShowWishlist');
+  };
+
+  const handleShowMyInfoPage = () => {
+    navigate('/MyInfo');
+  };
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('userId');
+    navigate('/login');
+  };
+
+  const handleProductManagement = () => {
+    navigate('/ProductManagement');
+  };
+
+  const handleChangeSearchTerm = (event) => {
+    setSearchTerm(event.target.value);
+    setSearchError('');
+  };
+
+  const handleEnterKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearchProduct();
+    }
+  };
+
+
+
+  // 컴포넌트가 마운트되면 사용자 ID와 검색어 목록을 가져옴
+  useEffect(() => {
+    fetchUserId();
+  }, [fetchUserId]);
+
+  useEffect(() => {
+    if (userId !== '') {
+      fetchSearchKeywords();
+    }
+  }, [userId, fetchSearchKeywords]);
+
+  return (
+    <div className="container-main">
+      <Header
+        toggleNavMenu={toggleNavMenu}
+        showNavMenu={showNavMenu}
+        closeNavMenu={closeNavMenu}
+        handleAddProduct={handleAddProduct}
+        handleShowChatList={handleShowChatList}
+        handleShowMyInfoPage={handleShowMyInfoPage}
+        handleKeywordManagement={handleKeywordManagement}
+        handleProductManagement={handleProductManagement}
+        handleLogout={handleLogout}
+        searchTerm={searchTerm}
+        handleChangeSearchTerm={handleChangeSearchTerm}
+        handleEnterKeyPress={handleEnterKeyPress}
+        searchInputRef={searchInputRef}
+        handleShowWishlist={handleShowWishlist}
+      />
+
+      <h1 className="search-keyword-header">검색 기록</h1>
+      <div className="search-keyword-container">
+        {searchKeywords.map((keyword) => (
+          <li key={keyword.id} className="keyword-item">
+            <span className="keyword-text">{keyword.search_term}</span>
+            <button className="delete-button" onClick={() => deleteKeyword(keyword.id)}>삭제</button>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SearchKeyword;
