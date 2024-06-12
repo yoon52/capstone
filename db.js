bff87c37a8d3f6d8c9f9fc8c22986dfb1b9927aa
SEUNGH00N <93211110+SEUNGH00N@users.noreply.github.com>
2024-06-12 16:58:38 +0900

    Update db.js

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2/promise');
-const bcrypt = require('bcrypt');
 const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
 const pathId = require('path');
+require('dotenv').config();
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 // MySQL 풀 생성
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '12345678',
-  database: 'capstone',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -40,7 +40,9 @@ const storageId = multer.diskStorage({
     cb(null, 'uploads_id/'); // 업로드된 파일이 저장될 디렉토리
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname); // 저장될 파일의 이름 설정
+    // 파일의 확장자를 jpg로 변경하여 저장
+    const extname = '.jpg'; // 확장자를 .jpg로 고정
+    cb(null, `${file.originalname.split('.')[0]}${extname}`);
   }
 });
 
@@ -55,6 +57,5 @@ app.use(cors());
 const upload = multer({ storage: storage });
 const uploadId = multer({ storage: storageId });
 
-
 // Export necessary modules
-module.exports = { app, pool, PORT, upload, uploadId, path, pathId};
+module.exports = { app, pool, PORT, upload, uploadId, path, pathId };
